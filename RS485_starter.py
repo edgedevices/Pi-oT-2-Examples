c70773890236caf35cb5c88ed331df8b3d342db6
Nate Albright <52329099+natealbright@users.noreply.github.com>
2022-02-07 12:05:57 -0500

    Update RS485_starter.py

--- RS485_starter.py
+++ RS485_starter.py
@@ -1,11 +1,14 @@
 import minimalmodbus
+from time import sleep
 
 ins = minimalmodbus.Instrument('/dev/ttyS0', 1, debug = True)
 ins.serial.timeout = 2
 ins.serial.baudrate = 9600
 
 while True:
-    temp = ins.read_register(0,1)
-    RH = ins.read_register(1,1)
-    print(temp,RH)
-
+    try:
+        temp = ins.read_register(0,1)
+        RH = ins.read_register(1,1)
+        print(temp,RH)
+    except IOError:
+        print("Failed to read from instrument")
