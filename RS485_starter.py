822393015d817a01286a99884ba3b90bd5814734
Nate Albright <52329099+natealbright@users.noreply.github.com>
2022-02-07 12:16:25 -0500

    Update RS485_starter.py

--- RS485_starter.py
+++ RS485_starter.py
@@ -1,14 +1,17 @@
+##This script uses the RS485 port on the Pi-oT 2 to read a modbus temperature and humidity sensor.
+##Depending on the slave device being used, the slave address, register
+
 import minimalmodbus
 from time import sleep
 
-ins = minimalmodbus.Instrument('/dev/ttyS0', 1, debug = True)
+ins = minimalmodbus.Instrument('/dev/ttyS0', 1)
 ins.serial.timeout = 2
 ins.serial.baudrate = 9600
 
 while True:
     try:
-        temp = ins.read_register(0,1)
-        RH = ins.read_register(1,1)
+        temp = ins.read_register(0,1) #register number=0 , number of decimals=1
+        RH = ins.read_register(1,1) #register number=1 , number of decimals=1
         print(temp,RH)
         sleep(1)
     except IOError:
